63bea03c45ff775cafcd06d538fcafdf5c0546bf
Lauri Suomalainen <>
2013-03-03 17:51:08 +0200

    Kaikki konstruktorit. Layer-luokka.

--- src/drawingtable/BasePicture.java
+++ src/drawingtable/BasePicture.java
@@ -9,7 +9,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
-
+import java.util.Stack;
 /**
  *
  * @author Lauri Suomalainen
@@ -17,6 +17,7 @@ import java.io.IOException;
 public class BasePicture {
     
     private BufferedImage picture;
+    private Stack layerStack = new Stack<Layer>();
     
     public BasePicture(File f) {
         picture = null;
@@ -27,6 +28,19 @@ public class BasePicture {
             System.exit(0); // return joko viesti tai blanko kuva.
     }
 }
+    public BasePicture (String FileName) {
+        picture = null;
+        try {
+            picture = ImageIO.read(new File(FileName));
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0); // return joko viesti tai blanko kuva.
+    }
+    }
+    public BasePicture() {
+        //picture = blank.jpg tai jotain
+        
+    }
 }
 
 
---
+++ src/drawingtable/Layer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package drawingtable;
+
+/**
+ *
+ * @author Lauri Suomalainen
+ */
+class Layer {
+
+}
