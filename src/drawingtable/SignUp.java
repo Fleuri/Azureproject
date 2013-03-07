b31e88401cb3b217a752268d6fb746c60d68eb17
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-03-07 18:55:46 +0200

    Update SignUp.java

--- src/drawingtable/SignUp.java
+++ src/drawingtable/SignUp.java
@@ -23,10 +23,11 @@ import javax.swing.WindowConstants;
  */
 public class SignUp extends JFrame implements ActionListener {
 
-    JButton SUBMIT;
+    JButton Register;
     JPanel panel;
-    JLabel label1, label2;
-    final JTextField text1, text2;
+    JLabel label1, label2, label3;
+    final JTextField text1, text2, text3;
+    boolean rightPass;
 
     SignUp() {
 
@@ -36,30 +37,45 @@ public class SignUp extends JFrame implements ActionListener {
 
         label2 = new JLabel();
         label2.setText("Password:");
-        text2 = new JTextField(15);
+        text2 = new JPasswordField(15);
+
+        label3 = new JLabel();
+        label3.setText("Retype Password:");
+        text3 = new JPasswordField(15);
 
         JButton sign = new JButton();
         sign.setText("Cancel");
+        
+        rightPass = false;
 
 
-        SUBMIT = new JButton("Register");
+        Register = new JButton("Register");
 
-        panel = new JPanel(new GridLayout(3, 1));
+        panel = new JPanel(new GridLayout(4, 1));
         panel.add(label1);
         panel.add(text1);
         panel.add(label2);
         panel.add(text2);
-        panel.add(SUBMIT);
+        panel.add(label3);
+        panel.add(text3);
+        panel.add(Register);
         panel.add(sign);
         add(panel, BorderLayout.CENTER);
-        SUBMIT.addActionListener(this);
+        Register.addActionListener(this);
         setTitle("Give new username and password, please");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public void actionPerformed(ActionEvent ae) {
-//        String value1 = text1.getText();
-//        String value2 = text2.getText();
+        String value1 = text1.getText();
+        String value2 = text2.getText();
+        String value3 = text3.getText();
+        if (value2.equals(value3)) {
+            rightPass = true;
+        }
+        else {
+            rightPass = false;
+        }
 //        if (value1.equals("roseindia") && value2.equals("roseindia")) {
 //            panel.removeAll();
 //            GUI gui = new GUI();
